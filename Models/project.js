005bca14fd3ead8fcf9966a21e56d9b7db51a7b8
jenna <parkerjenna98@gmail.com>
2023-02-23 13:37:26 -0500

    makes project shema categories same as frontend

--- Models/project.js
+++ Models/project.js
@@ -3,9 +3,24 @@ const { default: mongoose } = require("mongoose")
 const projectSchema = new mongoose.Schema({
     title: String,
     description: String,
-    tech: [{
-        name: String
-    }]
+    skills: [{
+        type: String
+    }],
+    roles: [{
+        type: String
+    }],
+    categories: [{
+        type: String
+    }],
+    figmaLink: String,
+    gitHubFrontendLink: String,
+    gitHubLink: String,
+    googleDriveLinks: String,
+    jiraLink: String,
+    microsoftTeamsLinks: String,
+    slackLink: String,
+    trelloLink: String,
+    zoomLink: String
 })
 
 const Project = mongoose.model("Project", projectSchema )
